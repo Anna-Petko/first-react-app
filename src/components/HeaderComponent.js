9c84b87eedfff3b534d3736d6ffcedfeeb5ea194
Анна Петько <annapetko@icloud.com>
2018-07-26 18:03:59 +0200

    Uncontrolled Form

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,5 +1,6 @@
  import React, {Component} from 'react';
- import { Navbar, NavbarBrand, Nav, NavbarToggler, Collapse, NavItem, Jumbotron } from 'reactstrap';
+ import { Navbar, NavbarBrand, Nav, NavbarToggler, Collapse, NavItem,
+        Jumbotron, Button,Modal, ModalHeader, ModalBody, Form, FormGroup,Label, Input } from 'reactstrap';
  import  {NavLink} from 'react-router-dom';
 
 
@@ -8,10 +9,14 @@
     constructor (props) {
         super(props);
         this.state = {
-            isNavOpen: false
+            isNavOpen: false,
+            isModalOpen: false
         };
 
         this.toggleNav =this.toggleNav.bind(this);
+        this.toggleModal =this.toggleModal.bind(this);
+        this.handleLogin =this.handleLogin.bind(this);
+
     }
 
     toggleNav() {
@@ -19,6 +24,20 @@
             isNavOpen: !this.state.isNavOpen
 
         });
+    }
+
+    toggleModal() {
+        this.setState({
+            isModalOpen: !this.state.isModalOpen
+
+        });
+    }
+
+    handleLogin(event) {
+        this.toggleModal();
+        alert("Username:" + this.username.value + "Password: " + this.password.value
+            + "Remember: " + this.remember.checked);
+        event.preventDefault();
     }
 
  	render() {
@@ -45,6 +64,13 @@
                                 <NavLink className="nav-link" to='/contactus'><span className="fa fa-address-card fa-lg"></span> Contact Us</NavLink>
                             </NavItem>
                             </Nav>
+                             <Nav className = "ml-auto" navbar>
+                                <NavItem>
+                                    <Button outline onClick = {this.toggleModal}>
+                                    <span className = "fa fa-sign-in fa-lg"></span> Login
+                                    </Button>
+                                </NavItem>
+                            </Nav>
                         </Collapse>
                     </div>
                 </Navbar>
@@ -58,6 +84,34 @@
                         </div>
                     </div>
                 </Jumbotron>
+                <Modal isOpen = {this.state.isModalOpen} toggle = {this.toggleModal}>
+                    <ModalHeader toggle = {this.toggleModal}>Login</ModalHeader>
+                    <ModalBody>
+                        <Form onSubmit = {this.handleLogin}>
+                            <FormGroup>
+                                <Label htmlFor="username">Username</Label>
+                                <Input type = "text" id = "username" name = "username" 
+                                inneRef={(input) =>this.username =input}/>
+                            </FormGroup>
+                            <FormGroup>
+                                <Label htmlFor="password">Password</Label>
+                                <Input type = "password" id = "password" name = "password" 
+                                inneRef={(input) =>this.password =input}/>
+                            </FormGroup>
+                            <FormGroup check>
+                                <Label check>
+                                    <Input type="checkbox" name = "remember"
+                                    inneRef={(input) =>this.remember =input} />
+                                    Remember me
+                                </Label>
+                            </FormGroup>
+                            <Button type ="submit" value = "submit" color="primary">
+                                Login
+                            </Button>
+                        </Form>
+                    </ModalBody>
+
+                </Modal>
             </div>
        </React.Fragment>
 
