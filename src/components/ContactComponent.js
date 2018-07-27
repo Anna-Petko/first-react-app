11ae42561bd31cf80d16c47aced9d5c38e9908a2
Анна Петько <annapetko@icloud.com>
2018-07-27 23:54:05 +0200

    Intro. to Redux

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -66,12 +66,12 @@ class Contact extends Component {
             email: ''
         };
 
-         if (this.state.touched.firstname && firstname.length < 3)
+        if (this.state.touched.firstname && firstname.length < 3)
             errors.firstname = 'First Name should be >= 3 characters';
         else if (this.state.touched.firstname && firstname.length > 10)
             errors.firstname = 'First Name should be <= 10 characters';
 
-         if (this.state.touched.lastname && lastname.length < 3)
+        if (this.state.touched.lastname && lastname.length < 3)
             errors.lastname = 'Last Name should be >= 3 characters';
         else if (this.state.touched.lastname && lastname.length > 10)
             errors.lastname = 'Last Name should be <= 10 characters';
